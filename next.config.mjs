8c19ddd0ff484ffab22317d6afb839b43ee26702
Milena <anticz.milena@gmail.com>
2025-03-11 17:35:32 +0100

    update

--- next.config.mjs
+++ next.config.mjs
@@ -14,9 +14,6 @@ import { unifiedConditional } from 'unified-conditional'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
-  basePath: "/djzk-law",
-  output: "export",
-  distDir: "dist"
 }
 
 function remarkMDXLayout(source, metaName) {
